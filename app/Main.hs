92e2813a1ee9b40aca07831c88b6c01621a6bbf7
Mitchell Holt <mitchellholt2002@gmail.com>
2023-12-02 13:46:12 +1000

    started project skeleton

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,29 @@
 module Main where
 
+import System.Environment
+import System.Exit
+
+data Filetype = Haskell | Rust | C deriving Show
+
+data Args = Args
+    { inFile :: String
+    , outFile :: String
+    , fileType :: Filetype
+    , debugOutput :: Bool
+    } deriving Show
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    parsedArgs <- processArgs <$> getArgs
+    case parsedArgs of
+        Left errStr -> putStrLn errStr
+        Right args -> print args
+
+
+-- Check that the two mandatory arguments inFile, outFile (in that order) are
+--  included, also check for a debug flag (-g or --debug) which will determine
+--  if the intermediate language is printed to stdout. The fileType will be
+--  determined by parsing fileType into the format <name>.<type>
+-- If for any reason parsing fails, create return an appropriate error string
+processArgs :: [String] -> Either String Args
+processArgs = undefined
